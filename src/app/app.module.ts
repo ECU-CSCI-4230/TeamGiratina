0fb4084b578184ac4637a243be0045a5dd9a6658
Brandon Suncin <brandon_suncin@outlook.com>
2019-02-26 12:07:56 -0500

    Recipe stuff
    The done module needs back end stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// services
+import { RecipeService } from './services/recipe.service';
+
+// components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditRecipeComponent } from './pages/edit-recipe/edit-recipe.component';
+import { NewRecipeComponent } from './pages/new-recipe/new-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditRecipeComponent,
+    NewRecipeComponent,
+    ShowRecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    RecipeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
