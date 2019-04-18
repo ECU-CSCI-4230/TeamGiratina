ab2bc6c0f8d007e2d6cba8682e3b159925b67358
tlp319 <tlp319@gmail.com>
2019-04-18 01:10:51 -0400

    Demo Ready!
    This build of PushNotifications is demo ready!

--- PushNotifications/PushNotifications/SendSMS/ejbModule/DocMaker.java
+++ PushNotifications/PushNotifications/SendSMS/ejbModule/DocMaker.java
@@ -1,28 +1,17 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
-import com.mongodb.client.MongoCursor;
 
 public class DocMaker {
 
-	public static void main(String[] args) {
+	public static DBCursor main(String[] args) {
 		MongoClient mongoClient = connectToServer();
 		DB db = mongoClient.getDB("node-mongo-registration-login-api");
 		BasicDBObject query = new BasicDBObject("notify", true);
 		DBCursor cursor = db.getCollection("users").find(query);
-		System.out.println(cursor.count());
-		while(cursor.hasNext()) {
-			System.out.println(cursor.next());
-		}	
-
+		return cursor;
 	}
 	
 	private static MongoClient connectToServer() {
